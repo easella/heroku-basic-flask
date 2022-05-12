16298d3a2b43f479d64670a29270cd75ed40e885
easella <74786054+easella@users.noreply.github.com>
2022-05-12 13:18:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,4 @@
-from flask import Flask
-import jinja2
-from requests import get
-from flask_redis import FlaskRedis
-app = Flask(__name__)
-redis_client = FlaskRedis(app)
+from editor import run_notebook
 
-SITE_NAME = 'https://games.awdrgyjil1234.repl.co/'
-
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>')
-def proxy(path):
-  resp = get(f'{SITE_NAME}{path}')
-  if resp.headers.get('content-type'):
-    return resp.content
-  return resp.text
-  
 if __name__ == '__main__':
-  app.jinja_env.cache = {}
-  app.run(host='0.0.0.0', port=8080)
+    run_notebook('demo.ipynb')
